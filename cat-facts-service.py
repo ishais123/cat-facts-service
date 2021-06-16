e00229c5ad1252f62966bff940cef066124f3936
ishai shahak <ishai@cloudride.co.il>
2021-06-16 17:57:58 +0300

    add default facts

--- cat-facts-service.py
+++ cat-facts-service.py
@@ -3,7 +3,7 @@ from flask import Flask
 import requests
 import json
 
-CAT_FACTS_URL = 'https://cat-fact.herokuapp.com/factss/random?amount=1'
+CAT_FACTS_URL = 'https://cat-fact.herokuapp.com/facts/random?amount=1'
 DEFAULT_FACTS = {
     "Status": "No response from API",
     "DefaultFact": "Cats are cute"
